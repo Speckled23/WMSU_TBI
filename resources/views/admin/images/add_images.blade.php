b3c42d41e483d5bd733483bb70fb94285343be10
Kaitlyn Mira <122985039+Speckled23@users.noreply.github.com>
2024-02-13 09:11:33 +0800

    first push

--- resources/views/admin/images/add_images.blade.php
+++ resources/views/admin/images/add_images.blade.php
@@ -14,12 +14,7 @@
                                 <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                                 <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
                                 </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                    <a class="dropdown-item" href="#">January - March</a>
-                                    <a class="dropdown-item" href="#">March - June</a>
-                                    <a class="dropdown-item" href="#">June - August</a>
-                                    <a class="dropdown-item" href="#">August - November</a>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
